0d9b704bbe29b88829c1254df76d3ef3934acf52
Yared <yayeyaye6@gmail.com>
2020-02-28 23:18:11 +0100

    if statement to save numbers

--- handlers/save-number.js
+++ handlers/save-number.js
@@ -1,15 +1,24 @@
 function saveNumberHandler() {
   debugger;
   // read new number from user input
-
+  const newInput = Number(document.getElementById("input").value);
+  numbers.all.push(numbers.current);
   // read from state the data you will need for the next step
 
   // find the new biggest and smallest values
-
+  if (numbers.smallest == Infinity){
+    numbers.smallest = newInput;
+  }  
+  if (newInput > numbers.biggest){
+    numbers.biggest = newInput;
+  } else if(newInput < numbers.smallest){
+    numbers.smallest = newInput;
+  }
   // update state: new biggest, new smallest, new current & save the last current
 
   // re-render the user interface with values stored in state
-
+  document.getElementById("biggest").innerText = numbers.biggest;
+  document.getElementById("smallest").innerText = numbers.smallest;
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'save number',
