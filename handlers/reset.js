71cbedd4c216b793dcce05310266e051d668bbfd
Yared <yayeyaye6@gmail.com>
2020-02-28 23:11:10 +0100

    code to handler

--- handlers/reset.js
+++ handlers/reset.js
@@ -1,9 +1,14 @@
 function resetHandler() {
   debugger;
   // reset state to initial values
-
+  numbers.current = 0;
+  numbers.all = [];
+  numbers.biggest = -Infinity;
+  numbers.smallest = Infinity;
   // re-render UI with values saved in state
-
+  document.getElementById("input").value = numbers.current;
+  document.getElementById("biggest").innerHTML = numbers.biggest;
+  document.getElementById("smallest").innerHTML = numbers.smallest;
   // log interaction: handler name, new state
   log.push({
     handler: 'reset',
@@ -11,3 +16,5 @@ function resetHandler() {
   })
 
 }
+
+  
